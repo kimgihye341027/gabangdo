d9ce86895ce7df97ac1ed6c94a28c898e58896ce
kimgihye <kimgihye55@gmail.com>
2025-03-28 10:24:26 +0900

    메인페이지 contents3 작업예정

--- kabangdo_vue/src/views/Content1.vue
+++ kabangdo_vue/src/views/Content1.vue
@@ -1,103 +1,99 @@
 <template>
-  <div class="macontent1-wrap">
+  <!-- 메인 컨텐츠1 전체영역 -->
+  <div class="macontent1_wrap">
     <div class="inner">
       <!-- 위 -->
-      <div class="macontent1-up">
-        <h3>| 짐운송도! 한번에!</h3>
+      <div class="macontent1_up">
+        <h3>|&nbsp;&nbsp;짐운송도! 한번에!</h3>
         <h4>가방도 하나면 끝!</h4>
       </div>
       <!-- 아래 -->
-      <div class="macontent1-down">
+      <div class="macontent1_down">
         <!-- s사이즈 -->
         <div class="masize">
           <!-- 사이즈 섹션 상단 설명 -->
-          <div class="masize-txt">
-            <h3>S 사이즈</h3>
-            <h4>기내용 캐리어, 소형 배낭 등</h4>
+          <div class="masize_txt">
+            <h2>S 사이즈</h2>
+            <h4>기내용 캐리어,소형 배낭 등</h4>
             <h5>21인치 이하/55cm 이하/10kg 미만</h5>
           </div>
           <!-- 아이콘 -->
-          <img
-            src="/public/images/ma/ma_content1/s_size.png"
-            alt="S사이즈"
-            class="masize-icon" />
+          <div class="masize_icon">
+            <img src="/public/images/ma/ma_content1/s_size.png" alt="S사이즈" />
+          </div>
           <!-- 사전예약시 -->
-          <div class="masize-beforecash">
+          <div class="masize_beforecash">
             <h5>사전예약시 10,000원</h5>
           </div>
           <!-- 당일예약시 -->
-          <div class="masize-aftercash">
+          <div class="masize_aftercash">
             <h4>당일예약</h4>
-            <h3>12,000원</h3>
+            <h2>12,000원</h2>
           </div>
         </div>
         <!-- m사이즈 -->
         <div class="masize">
           <!-- 사이즈 섹션 상단 설명 -->
-          <div class="masize-txt">
-            <h3>M 사이즈</h3>
-            <h4>화물용 캐리어, 등산가방 등</h4>
-            <h5>21인치 이하/65cm 미만/10kg 미만</h5>
+          <div class="masize_txt">
+            <h2>M 사이즈</h2>
+            <h4>화물용 캐리어,등산 가방 등</h4>
+            <h5>25인치 이하 / 65cm이하 / 20kg 미만</h5>
           </div>
           <!-- 아이콘 -->
-          <img
-            src="/public/images/ma/ma_content1/m_size.png"
-            alt="m사이즈"
-            class="masize-icon" />
+          <div class="masize_icon">
+            <img src="/public/images/ma/ma_content1/m_size.png" alt="m사이즈" />
+          </div>
           <!-- 사전예약시 -->
-          <div class="masize-beforecash">
+          <div class="masize_beforecash">
             <h5>사전예약시 14,000원</h5>
           </div>
           <!-- 당일예약시 -->
-          <div class="masize-aftercash">
+          <div class="masize_aftercash">
             <h4>당일예약</h4>
-            <h3>16,000원</h3>
+            <h2>16,000원</h2>
           </div>
         </div>
         <!-- l사이즈 -->
         <div class="masize">
           <!-- 사이즈 섹션 상단 설명 -->
-          <div class="masize-txt">
-            <h3>M 사이즈</h3>
-            <h4>화물용 캐리어, 등산가방 등</h4>
-            <h5>21인치 이하/65cm 미만/10kg 미만</h5>
+          <div class="masize_txt">
+            <h2>L 사이즈</h2>
+            <h4>대형 캐리어,골프백,대형 배낭 등</h4>
+            <h5>26인치 이상 / 66cm 이상 / 30kg 미만</h5>
           </div>
           <!-- 아이콘 -->
-          <img
-            src="/public/images/ma/ma_content1/m_size.png"
-            alt="m사이즈"
-            class="masize-icon" />
+          <div class="masize_icon">
+            <img src="/public/images/ma/ma_content1/l-size.png" alt="l사이즈" />
+          </div>
           <!-- 사전예약시 -->
-          <div class="masize-beforecash">
-            <h5>사전예약시 14,000원</h5>
+          <div class="masize_beforecash">
+            <h5>사전예약시 16,000원</h5>
           </div>
           <!-- 당일예약시 -->
-          <div class="masize-aftercash">
+          <div class="masize_aftercash">
             <h4>당일예약</h4>
-            <h3>16,000원</h3>
+            <h2>18,000원</h2>
           </div>
         </div>
         <!-- ?사이즈 -->
         <div class="masize">
           <!-- 사이즈 섹션 상단 설명 -->
-          <div class="masize-txt">
-            <h3>M 사이즈</h3>
-            <h4>화물용 캐리어, 등산가방 등</h4>
-            <h5>21인치 이하/65cm 미만/10kg 미만</h5>
+          <div class="masize_txt">
+            <h2>? 사이즈</h2>
+            <h4>기타 물품 및 표시 외 사이즈</h4>
+            <h5>가방도 문의 후 협의된 물품에 한함</h5>
           </div>
           <!-- 아이콘 -->
-          <img
-            src="/public/images/ma/ma_content1/m_size.png"
-            alt="m사이즈"
-            class="masize-icon" />
-          <!-- 사전예약시 -->
-          <div class="masize-beforecash">
-            <h5>사전예약시 14,000원</h5>
+          <div class="masize_icon">
+            <img src="/public/images/ma/ma_content1/size.png" alt="?사이즈" />
           </div>
-          <!-- 당일예약시 -->
-          <div class="masize-aftercash">
-            <h4>당일예약</h4>
-            <h3>16,000원</h3>
+          <!-- 사전예약시 -->
+          <div class="masize_beforecash">
+            <h5></h5>
+          </div>
+          <!-- 카카오톡 문의 -->
+          <div class="masize_aftercash">
+            <h2>카카오톡 문의</h2>
           </div>
         </div>
       </div>
@@ -105,4 +101,71 @@
   </div>
 </template>
 <script setup></script>
-<style scoped></style>
+<style scoped>
+/* 전체영역 */
+.macontent1_wrap {
+  width: 100%;
+  background-color: #0066b3;
+  margin: auto;
+}
+.macontent1_wrap .inner {
+  margin-top: 50px;
+  max-width: 1300px;
+  padding: 5% 0%;
+}
+/* 위 - txt */
+.macontent1_up {
+  display: flex;
+  color: #fff;
+  align-items: flex-end;
+}
+.macontent1_up > h4 {
+  margin-left: 10px;
+  font-weight: 400;
+}
+/* 위 - img-캡션별 */
+/* 전체영역 */
+.macontent1_down {
+  display: flex;
+  padding-top: 3%;
+  justify-content: space-between;
+}
+/* 사이즈 섹션 */
+.masize {
+  min-width: 280px;
+  padding: 30px;
+  border-radius: 30px;
+  background-color: #fff;
+}
+/* 사이즈 섹션 상단 설명  */
+.masize_txt > h4 {
+  color: #162b50;
+}
+.masize_txt > h5 {
+  color: #5a5a5a;
+}
+/* 아이콘 */
+.masize_icon {
+  padding: 10px;
+  text-align: center;
+}
+.masize_icon > img {
+  width: 70%;
+}
+/* 사이즈 섹션 하단 설명 */
+.masize_beforecash {
+  text-align: right;
+  min-height: 20px;
+  letter-spacing: -1px;
+  color: #5a5a5a;
+}
+.masize_aftercash {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 5px;
+  line-height: 27px;
+  letter-spacing: -1px;
+  color: #162b50;
+}
+</style>
--- kabangdo_vue/src/views/Content2.vue
+++ kabangdo_vue/src/views/Content2.vue
@@ -1,3 +1,170 @@
-<template></template>
+<template>
+  <div class="macontent2_wrap">
+    <div class="inner">
+      <div class="macontent2_icon">
+        <!-- 1. 가방도 -->
+        <div class="macontent2_iconimg">
+          <a href="#">
+            <!-- 아이콘 이미지 -->
+            <img
+              src="/public/images/ma/ma_content2/gabangdo_icon.png"
+              alt="가방도 아이콘" />
+            <!-- hover시 보일 텍스트 -->
+            <div class="macontent2_icon_hover">
+              <h5>
+                짐운송, 보관,<br />차량 대절까지<br />당일 배송, 안전한 픽업
+              </h5>
+            </div>
+          </a>
+          <!-- 항상 보이는 텍스트 -->
+          <div class="macontent2_icontxt">
+            <h4>가방도</h4>
+          </div>
+        </div>
+        <!-- 2. 예약도 -->
+        <div class="macontent2_iconimg">
+          <a href="#">
+            <!-- 아이콘 이미지 -->
+            <img
+              src="/public/images/ma/ma_content2/reservation_icon.png"
+              alt="예약도 아이콘" />
+            <!-- hover시 보일 텍스트 -->
+            <div class="macontent2_icon_hover">
+              <h5>
+                특별 예약으로<br />
+                원하는 서비스를<br />
+                빠르게 이용하세요.
+              </h5>
+            </div>
+          </a>
+          <!-- 항상 보이는 텍스트 -->
+          <div class="macontent2_icontxt">
+            <h4>예약도</h4>
+          </div>
+        </div>
+        <!-- 3. 요금도 -->
+        <div class="macontent2_iconimg">
+          <a href="#">
+            <!-- 아이콘 이미지 -->
+            <img
+              src="/public/images/ma/ma_content2/cash_icon.png"
+              alt="요금도 아이콘" />
+            <!-- hover시 보일 텍스트 -->
+            <div class="macontent2_icon_hover">
+              <h5>
+                서비스 요금과 결제 방법<br />
+                전체적으로 확인하세요.
+              </h5>
+            </div>
+          </a>
+          <!-- 항상 보이는 텍스트 -->
+          <div class="macontent2_icontxt">
+            <h4>요금도</h4>
+          </div>
+        </div>
+        <!-- 4. 소통도 -->
+        <div class="macontent2_iconimg">
+          <a href="#">
+            <!-- 아이콘 이미지 -->
+            <img
+              src="/public/images/ma/ma_content2/comunity_icon.png"
+              alt="소통도 아이콘" />
+            <!-- hover시 보일 텍스트 -->
+            <div class="macontent2_icon_hover">
+              <h5>
+                공지사항과 커뮤니티에서<br />가방도 프로모션과 후기들을<br />만나보세요.
+              </h5>
+            </div>
+          </a>
+          <!-- 항상 보이는 텍스트 -->
+          <div class="macontent2_icontxt">
+            <h4>소통도</h4>
+          </div>
+        </div>
+        <!-- 5. 여행도 -->
+        <div class="macontent2_iconimg">
+          <a href="#">
+            <!-- 아이콘 이미지 -->
+            <img
+              src="/public/images/ma/ma_content2/trip_icon.png"
+              alt="여행도 아이콘" />
+            <!-- hover시 보일 텍스트 -->
+            <div class="macontent2_icon_hover">
+              <h5>대구 축제, 관광지, 맛집<br />정보를 만나보세요.</h5>
+            </div>
+          </a>
+          <!-- 항상 보이는 텍스트 -->
+          <div class="macontent2_icontxt">
+            <h4>여행도</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script setup></script>
-<style scoped></style>
+<style scoped>
+/* 전체영역 */
+.macontent2_wrap {
+  width: 100%;
+  margin: auto;
+}
+.macontent2_wrap .inner {
+  margin-top: 50px;
+  max-width: 1300px;
+  padding: 5%;
+}
+/* 아이콘 전체 영역 */
+.macontent2_icon {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  text-align: center;
+}
+
+/* 아이콘 이미지 박스 */
+.macontent2_iconimg {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px; /* 원하는 크기로 조정 */
+  cursor: pointer;
+}
+
+/* 아이콘 이미지 */
+.macontent2_iconimg img {
+  width: 100%;
+  transition: filter 0.5s ease-in-out;
+}
+
+/* hover 시 이미지 배경 검게 만들기 */
+.macontent2_iconimg:hover img {
+  filter: brightness(50%);
+}
+
+/* hover 시 텍스트 보이게 하기 */
+.macontent2_icon_hover {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  text-align: center;
+  color: white;
+  padding: 10px;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+
+/* hover 시 텍스트 보이게 설정 */
+.macontent2_iconimg:hover .macontent2_icon_hover {
+  opacity: 1;
+}
+
+/* 항상 보이는 텍스트 */
+.macontent2_icontxt h4 {
+  margin-top: 8px;
+  color: black;
+}
+</style>
